250a6b180c5e46f9a1c9f451e4659bb4a0955719
Layton <caolei6767@gmail.com>
2023-02-24 21:32:56 +0800

    add some keymap

--- lua/keymap/editor.lua
+++ lua/keymap/editor.lua
@@ -93,6 +93,8 @@ local plug_map = {
 	-- Plugin: tabout
 	["i|<A-l>"] = map_cmd("<Plug>(TaboutMulti)"):with_silent():with_noremap():with_desc("edit: Goto end of pair"),
 	["i|<A-h>"] = map_cmd("<Plug>(TaboutBackMulti)"):with_silent():with_noremap():with_desc("edit: Goto begin of pair"),
+
+	["v|p"] = map_cmd("pgvy"),
 }
 
 bind.nvim_load_mapping(plug_map)
