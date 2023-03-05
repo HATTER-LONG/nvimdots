0981f597ca84a3f4ffbe96676a1e99cafeef87e6
Layton <caolei6767@gmail.com>
2023-03-05 16:24:52 +0800

    update kanagawa

--- lua/modules/plugins/ui.lua
+++ lua/modules/plugins/ui.lua
@@ -19,7 +19,13 @@ ui["rebelot/kanagawa.nvim"] = {
 			transparent = false, -- do not set background color
 			dimInactive = true, -- dim inactive window `:h hl-NormalNC`
 			colors = {},
-			overrides = {},
+			-- overrides = {},
+
+			theme = "wave", -- Load "wave" theme when 'background' option is not set
+			background = { -- map the value of 'background' option to a theme
+				dark = "wave", -- try "dragon" !
+				light = "lotus",
+			},
 		})
 	end,
 }
