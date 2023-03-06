2a32d67a47e83f868ba52e61164741d60175cc8e
Layton <caolei6767@gmail.com>
2023-03-06 19:06:40 +0800

    update

--- lua/modules/plugins/tool.lua
+++ lua/modules/plugins/tool.lua
@@ -17,6 +17,33 @@ tool["folke/which-key.nvim"] = {
 -- 	cond = vim.fn.executable("fcitx5-remote") == 1,
 -- 	config = require("tool.fcitx5"),
 -- }
+tool["brglng/vim-im-select"] = {
+	opt = true,
+	event = "BufReadPost",
+	config = function()
+		local global = require("core.global")
+		if global.is_mac then
+			vim.api.nvim_command([[let g:im_select_get_im_cmd = ["macism"] ]])
+			vim.api.nvim_command([[let g:im_select_default = "com.apple.keylayout.ABC"]])
+			vim.api.nvim_command([[let g:ImSelectSetImCmd = {key -> ['macism', key]}]])
+			vim.api.nvim_command([[let g:im_select_enable_focus_events = 0]])
+		elseif vim.fn.executable("fcitx5-remote") == 1 then
+			-- fcitx5 need a manual config
+			vim.api.nvim_cmd({
+				[[ let g:im_select_get_im_cmd = ["fcitx5-remote"] ]],
+				[[ let g:im_select_default = '1' ]],
+				[[ let g:ImSelectSetImCmd = {
+			\ key ->
+			\ key == 1 ? ['fcitx5-remote', '-c'] :
+			\ key == 2 ? ['fcitx5-remote', '-o'] :
+			\ key == 0 ? ['fcitx5-remote', '-c'] :
+			\ execute("throw 'invalid im key'")
+			\ }
+			]],
+			}, { true, true, true })
+		end
+	end,
+}
 tool["nvim-tree/nvim-tree.lua"] = {
 	lazy = true,
 	cmd = {
