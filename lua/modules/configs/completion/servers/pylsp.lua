4e53a9880d466e5907254ed79d9b2b4a929b1e05
Layton <caolei6767@gmail.com>
2023-04-01 16:04:10 +0800

    update

---
+++ lua/modules/configs/completion/servers/pylsp.lua
@@ -0,0 +1,14 @@
+return function(options)
+	require("lspconfig").pylsp.setup({
+		settings = {
+			pylsp = {
+				plugins = {
+					pycodestyle = {
+						ignore = { "W391" },
+						maxLineLength = 100,
+					},
+				},
+			},
+		},
+	})
+end
