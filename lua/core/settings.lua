2beb4b082bf65fb3588376c5be5b01219a1d7162
Layton <caolei6767@gmail.com>
2023-04-08 18:04:28 +0800

    update

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -77,7 +77,7 @@ settings["lsp_deps"] = {
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins
 ---@type string[]
 settings["null_ls_deps"] = {
-	"black",
+	-- "black",
 	"clang_format",
 	"editorconfig_checker",
 	"prettier",
