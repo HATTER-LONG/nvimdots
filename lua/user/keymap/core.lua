8a75d0128ec28936a65218acc4055b061f81315a
Layton <caolei6767@gmail.com>
2023-08-26 21:54:44 +0800

    update keymap

--- lua/user/keymap/core.lua
+++ lua/user/keymap/core.lua
@@ -1,5 +1,31 @@
+local map_cmd = require("keymap.bind").map_cmd
+local map_callback = require("keymap.bind").map_callback
 return {
 	["n|<A-d>"] = false,
 	["i|<A-d>"] = false,
 	["t|<A-d>"] = false,
+
+	["n|<C-h>"] = map_callback(function()
+			require("smart-splits").move_cursor_left()
+		end)
+		:with_noremap()
+		:with_desc("window: Focus left"),
+
+	["n|<C-l>"] = map_callback(function()
+			require("smart-splits").move_cursor_right()
+		end)
+		:with_noremap()
+		:with_desc("window: Focus right"),
+
+	["n|<C-j>"] = map_callback(function()
+			require("smart-splits").move_cursor_down()
+		end)
+		:with_noremap()
+		:with_desc("window: Focus down"),
+
+	["n|<C-k>"] = map_callback(function()
+			require("smart-splits").move_cursor_up()
+		end)
+		:with_noremap()
+		:with_desc("window: Focus up"),
 }
--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -19,4 +19,8 @@ ui["rebelot/kanagawa.nvim"] = {
 	end,
 }
 
+ui["mrjones2014/smart-splits.nvim"] = {
+	opts = { ignored_filetypes = { "nofile", "quickfix", "qf", "prompt" }, ignored_buftypes = { "nofile" } },
+}
+
 return ui
