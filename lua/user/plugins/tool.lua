bbbc192c6610bcd7f0360ca306544bdb65dd1e3a
Layton <caolei6767@gmail.com>
2024-04-05 22:03:29 +0800

    update some plugin

--- lua/user/plugins/tool.lua
+++ lua/user/plugins/tool.lua
@@ -254,13 +254,56 @@ else
 			"nvim-telescope/telescope.nvim",
 		},
 	}
-	tool["TabbyML/vim-tabby"] = {
-		-- event = { "LspAttach" },
-		-- lazy = true,
+	tool["huggingface/llm.nvim"] = {
+		event = "BufReadPost",
+		lazy = true,
 		config = function()
-			vim.g.tabby_keybinding_accept = "<C-l>"
+			local llm = require("llm")
+			llm.setup({
+				api_token = nil, -- cf Install paragraph
+				model = "starcoder:3b", -- the model ID, behavior depends on backend
+				backend = "ollama", -- backend ID, "huggingface" | "ollama" | "openai" | "tgi"
+				url = "http://ubuntu.lan:8080/api/generate", -- the http url of the backend
+				tokens_to_clear = { "<|endoftext|>" }, -- tokens to remove from the model's output
+				-- parameters that are added to the request body, values are arbitrary, you can set any field:value pair here it will be passed as is to the backend
+				request_body = {
+					parameters = {
+						max_new_tokens = 60,
+						temperature = 0.2,
+						top_p = 0.95,
+					},
+				},
+				-- set this if the model supports fill in the middle
+				fim = {
+					enabled = true,
+					prefix = "<fim_prefix>",
+					middle = "<fim_middle>",
+					suffix = "<fim_suffix>",
+				},
+				debounce_ms = 150,
+				accept_keymap = "<C-l>",
+				dismiss_keymap = "<C-\\>",
+				tls_skip_verify_insecure = false,
+				-- llm-ls configuration, cf llm-ls section
+				lsp = {
+					bin_path = nil,
+					host = nil,
+					port = nil,
+					version = "0.5.2",
+				},
+				tokenizer = nil, -- cf Tokenizer paragraph
+				context_window = 8192, -- max number of tokens for the context window
+				enable_suggestions_on_startup = true,
+				enable_suggestions_on_files = "*", -- pattern matching syntax to enable suggestions on specific files, either a string or a list of strings
+			})
 		end,
 	}
+	-- tool["TabbyML/vim-tabby"] = {
+	-- 	lazy = false,
+	-- 	config = function()
+	-- 		vim.g.tabby_keybinding_accept = "<C-l>"
+	-- 	end,
+	-- }
 end
 -- tool["postfen/clipboard-image.nvim"] = {
 -- 	lazy = true,
@@ -296,4 +339,9 @@ end
 -- 	end,
 -- }
 
+tool["ibhagwan/smartyank.nvim"] = {
+	lazy = true,
+	event = "BufReadPost",
+	config = require("user.configs.tool.smartyank"),
+}
 return tool
